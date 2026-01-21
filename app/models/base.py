5c1e06a8f99c4f813f9fba41ca50594ca41ec080
Blassel228 <hok.ykrop@gmail.com>
2026-01-21 17:53:19 +0200

    changing base created at

--- app/models/base.py
+++ app/models/base.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from decimal import Decimal
 from typing import Any
 from uuid import UUID, uuid4
 
-from sqlalchemy import Boolean, DateTime, String, func
+from sqlalchemy import Boolean, DateTime, String, func, text
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
@@ -13,8 +13,12 @@ class UUIDModel:
 
 
 class CreatedAtModel:
-    created_at: Mapped[datetime] = mapped_column(default=func.now(), server_default=func.now(), index=True)
-
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=text("TIMEZONE('utc', now())"),
+        default=lambda: datetime.now(timezone.utc),
+        index=True,
+    )
 
 class UpdatedAtModel:
     updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
